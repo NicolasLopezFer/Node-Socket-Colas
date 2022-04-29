c4e39cb4c92722c7c04e24b0df56160bdc76f1d6
Nicolas Lopez <nlopez97f@gmail.com>
2022-04-28 22:18:42 -0500

    hospital screen done

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 
 class Server {
@@ -7,6 +8,8 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {}
 
@@ -16,6 +19,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Sockets
+        this.sockets();
     }
 
 
@@ -35,8 +41,12 @@ class Server {
         
     }
 
+    sockets() {
+        this.io.on('connection', socketController);
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
---
+++ sockets/controller.js
@@ -0,0 +1,18 @@
+
+
+const socketController = (socket) => {
+
+    socket.on('enviar-mensaje', ( payload, callback ) => {
+        
+        const id = 123456;
+        callback( id );
+
+        socket.broadcast.emit('enviar-mensaje', payload );
+
+    });
+  }
+
+
+module.exports = {
+    socketController,
+}
